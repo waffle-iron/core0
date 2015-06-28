cc6419a0fefe1bb71e6533dec8f93a93ecf5212f
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-06-28 11:03:34 +0200

    Implement get_host_info

---
+++ agent/lib/builtin/get_host_info.go
@@ -0,0 +1,48 @@
+package builtin
+
+import (
+    "time"
+    "github.com/Jumpscale/jsagent/agent/lib/pm"
+    "github.com/shirou/gopsutil/host"
+    "encoding/json"
+)
+
+const (
+    CMD_GET_HOST_INFO = "get_host_info"
+)
+
+func init() {
+    pm.CMD_MAP[CMD_GET_HOST_INFO] = InternalProcessFactory(getHostInfo)
+}
+
+func getHostInfo(cmd *pm.Cmd, cfg pm.RunCfg) {
+    result := &pm.JobResult {
+        Id: cmd.Id,
+        Gid: cmd.Gid,
+        Nid: cmd.Nid,
+        Args: cmd.Args,
+        StartTime: time.Now().Unix(),
+        Level: pm.L_RESULT_JSON,
+    }
+
+    defer func() {
+        result.Time = time.Now().Unix() - result.StartTime
+    } ()
+
+    info, err := host.HostInfo()
+
+    if err != nil {
+        result.State = pm.S_ERROR
+        m, _ := json.Marshal(err)
+        result.Data = string(m)
+
+        return
+    }
+
+    result.State = pm.S_SUCCESS
+    m, _ := json.Marshal(info)
+
+    result.Data = string(m)
+
+    cfg.ResultHandler(result)
+}
