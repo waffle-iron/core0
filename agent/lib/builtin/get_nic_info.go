5ff78e9a79ae7b32e2b6a449940a13878ae3821a
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-06-28 11:27:31 +0200

    Implement get_nic_info

---
+++ agent/lib/builtin/get_nic_info.go
@@ -0,0 +1,48 @@
+package builtin
+
+import (
+    "time"
+    "github.com/Jumpscale/jsagent/agent/lib/pm"
+    "github.com/shirou/gopsutil/net"
+    "encoding/json"
+)
+
+const (
+    CMD_GET_NIC_INFO = "get_nic_info"
+)
+
+func init() {
+    pm.CMD_MAP[CMD_GET_NIC_INFO] = InternalProcessFactory(getNicInfo)
+}
+
+func getNicInfo(cmd *pm.Cmd, cfg pm.RunCfg) {
+    result := &pm.JobResult {
+        Id: cmd.Id,
+        Gid: cmd.Gid,
+        Nid: cmd.Nid,
+        Args: cmd.Args,
+        StartTime: time.Now().Unix(),
+        Level: pm.L_RESULT_JSON,
+    }
+
+    defer func() {
+        result.Time = time.Now().Unix() - result.StartTime
+    } ()
+
+    info, err := net.NetInterfaces()
+
+    if err != nil {
+        result.State = pm.S_ERROR
+        m, _ := json.Marshal(err)
+        result.Data = string(m)
+
+        return
+    }
+
+    result.State = pm.S_SUCCESS
+    m, _ := json.Marshal(info)
+
+    result.Data = string(m)
+
+    cfg.ResultHandler(result)
+}
