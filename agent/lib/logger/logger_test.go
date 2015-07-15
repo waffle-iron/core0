c548e738c62161e4a385192e838c5ec561d9ea5e
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-07-15 10:43:27 +0200

    More logger unittests

--- agent/lib/logger/logger_test.go
+++ agent/lib/logger/logger_test.go
@@ -2,7 +2,13 @@ package logger
 
 import (
     "os"
+    "sync"
+    "net"
+    "net/http"
     "testing"
+    "io/ioutil"
+    "encoding/json"
+    "time"
     "github.com/Jumpscale/jsagent/agent/lib/pm"
 )
 
@@ -166,5 +172,164 @@ func TestDBLogger_ForceLevel(t *testing.T) {
 
     if count != 1 {
         t.Error("Wrong number of records in DB", count)
+    }
+}
+
+
+func TestACLogger_BatchSizeTrigger(t *testing.T) {
+    var wg sync.WaitGroup
+
+    serverMux := http.NewServeMux()
+    server := &http.Server{
+        Handler: serverMux,
+    }
+
+    listner, err := net.Listen("tcp", ":1234")
+    if err != nil {
+        t.Error(err)
+    }
+
+    signal := make(chan int)
+    handle := func(writer http.ResponseWriter, request *http.Request) {
+        defer listner.Close()
+        defer request.Body.Close()
+        body, err := ioutil.ReadAll(request.Body)
+        if err != nil {
+            t.Error(err)
+        }
+
+        //content is the serialized log messages.
+        var messages []*pm.Message
+        json.Unmarshal(body, &messages)
+
+        signal <- len(messages)
+    }
+
+    wg.Add(1)
+    //starting proxy
+    go func () {
+        defer wg.Done()
+        serverMux.HandleFunc("/logs", handle)
+        go server.Serve(listner)
+    }()
+
+    //wait until proxy is ready before starting agents.
+    wg.Wait()
+
+    logger := NewACLogger([]string{"http://localhost:1234/logs"}, 2,
+        60 * time.Minute, []int{1, 2})
+
+    message1 := "Hello world"
+
+    msg1 := &pm.Message {
+        Id: 1,
+        Cmd: getFakeCmd(t),
+        Level: 1,
+        Message: message1,
+        Epoch: 1000,
+    }
+
+    msg2 := &pm.Message {
+        Id: 2,
+        Cmd: getFakeCmd(t),
+        Level: 1,
+        Message: message1,
+        Epoch: 1000,
+    }
+
+    logger.Log(msg1)
+    logger.Log(msg2)
+
+    select {
+    case l := <- signal:
+        if l != 2 {
+            t.Error("Invalid number of messages logged")
+        }
+    case <- time.After(10 * time.Second):
+        t.Error("Something went wrong, messages never received at the end point")
+    }
+}
+
+
+func TestACLogger_FlushIntTrigger(t *testing.T) {
+    var wg sync.WaitGroup
+
+    serverMux := http.NewServeMux()
+    server := &http.Server{
+        Handler: serverMux,
+    }
+
+    listner, err := net.Listen("tcp", ":1236")
+    if err != nil {
+        t.Error(err)
+    }
+
+    signal := make(chan int)
+    handle := func(writer http.ResponseWriter, request *http.Request) {
+        defer listner.Close()
+        defer request.Body.Close()
+        body, err := ioutil.ReadAll(request.Body)
+        if err != nil {
+            t.Error(err)
+        }
+
+        //content is the serialized log messages.
+        var messages []*pm.Message
+        json.Unmarshal(body, &messages)
+
+        signal <- len(messages)
+    }
+
+    wg.Add(1)
+    //starting proxy
+    go func () {
+        defer wg.Done()
+        serverMux.HandleFunc("/logs", handle)
+        go server.Serve(listner)
+    }()
+
+    //wait until proxy is ready before starting agents.
+    wg.Wait()
+
+    logger := NewACLogger([]string{"http://localhost:1236/logs"}, 100,
+        5 * time.Second, []int{1, 2})
+
+    message1 := "Hello world"
+
+    msg1 := &pm.Message {
+        Id: 1,
+        Cmd: getFakeCmd(t),
+        Level: 1,
+        Message: message1,
+        Epoch: 1000,
+    }
+
+    msg2 := &pm.Message {
+        Id: 2,
+        Cmd: getFakeCmd(t),
+        Level: 1,
+        Message: message1,
+        Epoch: 1000,
+    }
+
+    msg3 := &pm.Message {
+        Id: 2,
+        Cmd: getFakeCmd(t),
+        Level: 5,
+        Message: message1,
+        Epoch: 1000,
+    }
+
+    logger.Log(msg1)
+    logger.Log(msg2)
+    logger.Log(msg3)
+
+    select {
+    case l := <- signal:
+        if l != 2 {
+            t.Error("Invalid number of messages logged")
+        }
+    case <- time.After(10 * time.Second):
+        t.Error("Something went wrong, messages never received at the end point")
     }
 }
