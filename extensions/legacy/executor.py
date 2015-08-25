72122555322f94c347a2e55652c1b5d8f859a46e
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-08-25 13:07:39 +0200

    WIP: Executor daemon for jumpscripts. - I still have an issue with first exec for some reason the parent process never returns!

---
+++ extensions/legacy/executor.py
@@ -0,0 +1,104 @@
+import os
+import threading
+import cPickle as pickle
+import socket
+import errno
+import imp
+import sys
+import logging
+import struct
+
+
+class Connection(object):
+    HEADER_FMT = '@i'
+    HEADER_SIZE = struct.calcsize(HEADER_FMT)
+
+    def __init__(self, sock):
+        self.sock = sock
+
+    def _recv_size(self, size):
+        data = ''
+        while len(data) < size:
+            data += self.sock.recv(size - len(data))
+
+        return data
+
+    def receive(self):
+        header = self._recv_size(self.HEADER_SIZE)
+        size = struct.unpack(self.HEADER_FMT, header)[0]
+
+        raw_data = self._recv_size(size)
+        return pickle.loads(raw_data)
+
+    def send(self, message):
+        data = pickle.dumps(message)
+        header = struct.pack(self.HEADER_FMT, len(data))
+        self.sock.sendall(header)
+        self.sock.sendall(data)
+
+    def close(self):
+        self.sock.close()
+
+
+class WrapperThread(threading.Thread):
+    def __init__(self, con):
+        self.con = con
+        super(WrapperThread, self).__init__()
+
+    def run(self):
+        try:
+            data = self.con.receive()
+            jspath = os.environ.get('JUMPSCRIPTS_HOME')
+            path = os.path.join(jspath, data['domain'], '%s.py' % data['name'])
+            logging.info('Executing jumpscript %s' % path)
+
+            module = imp.load_source(path, path)
+            result = module.action(**data['args'])
+
+            self.con.send(result)
+        except Exception, e:
+            logging.error(e)
+            self.con.send(e)
+        finally:
+            self.con.close()
+
+
+def daemon(unix_sock_path):
+    sock = socket.socket(socket.AF_UNIX)
+    try:
+        os.unlink(unix_sock_path)
+    except OSError, e:
+        if e.errno != errno.ENOENT:
+            raise
+
+    sock.bind(unix_sock_path)
+    sock.listen(10)
+
+    # socket is ready, now do the daemon forking
+    pid = os.fork()
+    if pid > 0:
+        # parent.
+        os.wait()
+        return
+
+    os.setsid()
+    os.umask(0)
+    logging.basicConfig(filename='/var/log/legacy.log', level=logging.INFO)
+    # double fork to avoid zombies
+    logging.info('Starting executor daemon')
+
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # parent
+            sys.exit(0)
+    except OSError, e:
+        logging.error(e)
+        sys.exit(1)
+
+    # importing jumpscale
+    from JumpScale import j # NOQA
+
+    while True:
+        s = sock.accept()[0]
+        WrapperThread(Connection(s)).start()
--- extensions/legacy/wrapper.py
+++ extensions/legacy/wrapper.py
@@ -1,15 +1,52 @@
 import utils
 import os
-import imp
+import socket
+import errno
+import executor
 
 
-def wrapper(data):
+EXEC_SOCKET = '/tmp/legacy.sock'
+
+
+def runner(data):
     jspath = os.environ.get('JUMPSCRIPTS_HOME')
-    if jspath is None:
-        raise RuntimeError('Missconfigured, no jspath define')
+    assert jspath is not None, 'Missconfigured, no jspath define'
 
-    path = os.path.join(jspath, data['domain'], '%s.py' % data['name'])
-    module = imp.load_source(path, path)
-    return module.action(**data['args'])
+    sock = None
+    daemon_running = False
+    # we use the lock to avoid having multiple processes trying to
+    # start the executor at the same time.
 
-utils.run(wrapper)
+    with utils.Lock('/tmp/legacy.lock'):
+        trial = 0
+        while True:
+            try:
+                trial += 1
+                sock = socket.socket(socket.AF_UNIX)
+                sock.connect(EXEC_SOCKET)
+                break
+            except Exception, e:
+                if trial >= 3:
+                    raise Exception('Failed to establish connection with the executor')
+
+                if e.errno in (errno.ENOENT, errno.ECONNREFUSED):
+                    # start the executor deamon.
+                    if not daemon_running:
+                        executor.daemon(EXEC_SOCKET)
+                        daemon_running = True
+                else:
+                    raise
+
+    con = executor.Connection(sock)
+    con.send(data)
+
+    response = con.receive()
+
+    if isinstance(response, BaseException):
+        raise response
+
+    con.close()
+    return response
+
+
+utils.run(runner)
