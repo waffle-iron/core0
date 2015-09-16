a872a4c87e4b597eb3ace33fd4d60d1bfe51df9f
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-09-16 14:56:41 +0200

    Add syncthing extenstion to run the syncthing server as a process in the agent.

--- extensions/sync/get_id.py
+++ extensions/sync/get_id.py
@@ -1,15 +1,28 @@
 import utils
 import requests
-
+import logging
+import time
 import _sync as sync
 
 
 def get_id(data):
     url = sync.get_url(sync.ENDPOINT_STATUS)
 
-    response = requests.get(url)
-    if not response.ok:
-        raise Exception('Invalid response from syncthing: %s' % response.reason)
+    _errors = 0
+    while True:
+        try:
+            response = requests.get(url)
+            if not response.ok:
+                raise Exception('Invalid response from syncthing: %s' % response.reason)
+            else:
+                break
+        except:
+            _errors += 1
+            if _errors >= 3:
+                raise
+            seconds = 3 * _errors
+            logging.info('Error retreiving syncthing config, retrying in %s seconds', seconds)
+            time.sleep(seconds)
 
     result = response.json()
 
