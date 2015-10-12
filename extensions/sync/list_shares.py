ffcd7f1b43d91233a78371d1cd64a9721207b056
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-10-12 14:16:15 +0200

    Don't return jumpscripts folder because it's dangerous to miss with this one

--- extensions/sync/list_shares.py
+++ extensions/sync/list_shares.py
@@ -4,12 +4,23 @@ import api
 import _sync as sync
 
 
+INTERNAL = ('/opt/jumpscale7/apps/agent2/jumpscripts',)
+
+
 def list_shares(data):
     syncthing = api.Syncthing(sync.SYNCTHING_URL)
 
     config = syncthing.config
 
-    return config['folders']
+    folders = config['folders']
+
+    results = []
+    for folder in folders:
+        if folder['path'] in INTERNAL:
+            continue
+        results.append(folder)
+
+    return results
 
 if __name__ == '__main__':
     utils.run(list_shares)
