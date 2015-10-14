2b310d97b10fed8d73c448be21056b653a5bbbf2
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-10-14 12:58:46 +0200

    Clean up input streams

--- agent/lib/pm/input.go
+++ agent/lib/pm/input.go
@@ -27,77 +27,79 @@ func newStreamConsumer(cmd *Cmd, reader io.Reader, level int) *streamConsumer {
 	}
 }
 
-func (consumer *streamConsumer) Consume(handler MessageHandler) {
-	// read input until the end (or closed)
-	// process all messages as speced x:: or x:::
-	// other messages that has no level are assumed of level consumer.level
-	go func() {
-		reader := bufio.NewReader(consumer.reader)
-		var level int
-		var message string
-		var multiline = false
+// read input until the end (or closed)
+// process all messages as speced x:: or x:::
+// other messages that has no level are assumed of level consumer.level
+func (consumer *streamConsumer) consume(handler MessageHandler) {
+	reader := bufio.NewReader(consumer.reader)
+	var level int
+	var message string
+	var multiline = false
 
-		for {
-			line, err := reader.ReadString('\n')
+	for {
+		line, err := reader.ReadString('\n')
 
-			if err != nil && err != io.EOF {
-				log.Println(err)
-				return
-			}
+		if err != nil && err != io.EOF {
+			log.Println(err)
+			return
+		}
 
-			line = strings.TrimRight(line, "\n")
+		line = strings.TrimRight(line, "\n")
 
-			if line != "" {
-				if !multiline {
-					matches := pmMsgPattern.FindStringSubmatch(line)
-					if matches == nil {
-						//use default level.
-						handler(&Message{
-							Cmd:     consumer.cmd,
-							Level:   consumer.level,
-							Message: line,
-						})
-					} else {
-						l, _ := strconv.ParseInt(matches[1], 10, 0)
-						level = int(l)
-						message = matches[3]
-
-						if matches[2] == ":::" {
-							multiline = true
-						} else {
-							//single line message
-							handler(&Message{
-								Cmd:     consumer.cmd,
-								Level:   level,
-								Message: message,
-							})
-						}
-					}
+		if line != "" {
+			if !multiline {
+				matches := pmMsgPattern.FindStringSubmatch(line)
+				if matches == nil {
+					//use default level.
+					handler(&Message{
+						Cmd:     consumer.cmd,
+						Level:   consumer.level,
+						Message: line,
+					})
 				} else {
-					/*
-					   A known issue is that if stream was closed (EOF) before
-					   we receive the ::: termination of multiline string. We discard
-					   the uncomplete multiline string message.
-					*/
-					if line == ":::" {
-						multiline = false
-						//flush message
+					l, _ := strconv.ParseInt(matches[1], 10, 0)
+					level = int(l)
+					message = matches[3]
+
+					if matches[2] == ":::" {
+						multiline = true
+					} else {
+						//single line message
 						handler(&Message{
 							Cmd:     consumer.cmd,
 							Level:   level,
 							Message: message,
 						})
-					} else {
-						message += "\n" + line
 					}
 				}
-			}
-
-			if err == io.EOF {
-				consumer.Signal <- 1
-				close(consumer.Signal)
-				return
+			} else {
+				/*
+				   A known issue is that if stream was closed (EOF) before
+				   we receive the ::: termination of multiline string. We discard
+				   the uncomplete multiline string message.
+				*/
+				if line == ":::" {
+					multiline = false
+					//flush message
+					handler(&Message{
+						Cmd:     consumer.cmd,
+						Level:   level,
+						Message: message,
+					})
+				} else {
+					message += "\n" + line
+				}
 			}
 		}
-	}()
+
+		if err == io.EOF {
+			consumer.Signal <- 1
+			close(consumer.Signal)
+			return
+		}
+	}
+}
+
+func (consumer *streamConsumer) Consume(handler MessageHandler) {
+	go consumer.consume(handler)
 }
