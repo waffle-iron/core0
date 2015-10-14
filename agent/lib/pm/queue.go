6bbe272417401abc614253a730bc9f5a67dc2ca0
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-10-14 11:14:40 +0200

    cleaning up

--- agent/lib/pm/queue.go
+++ agent/lib/pm/queue.go
@@ -14,6 +14,7 @@ type cmdQueueManager struct {
 	signal   chan string
 	consumer chan *Cmd
 	producer chan *Cmd
+	lock     sync.Mutex
 }
 
 /*
@@ -27,73 +28,77 @@ func newCmdQueueManager() *cmdQueueManager {
 		producer: make(chan *Cmd),
 	}
 
-	var lock sync.Mutex
 	//start queue dispatcher
-	go func() {
-		for {
-			cmd := <-mgr.consumer
-
-			queueName := cmd.Args.GetString("queue")
-			if queueName == "" {
-				//not a queued command just log a warning and continue
-				log.Println("WARNING: Queue manager received a command with no set queue", cmd)
-				continue
-			}
-
-			log.Println("Pushing command to queue", queueName)
-			//push command to correct queue
-			lock.Lock()
-			queue, ok := mgr.queues[queueName]
-			if !ok {
-				log.Println("Queue", queueName, "doesn't exist, initializing...")
-				queue = list.New()
-				mgr.queues[queueName] = queue
-			}
-			lock.Unlock()
-			//push the command to the queue.
-			queue.PushBack(cmd)
-
-			if !ok {
-				//since we just create this queue. We signal that it's ready
-				//to produce the first queued command. Think of it as intial start
-				//condition. When this command exists, it will auto signal the next command
-				//and so on.
-				mgr.signal <- queueName
-			}
-		}
-	}()
+	go mgr.dispatcherLoop()
 
 	//start the producer
-	go func() {
-		for {
-			queueName := <-mgr.signal
-
-			log.Println("Queue", queueName, "ready...")
-			lock.Lock()
-			queue, ok := mgr.queues[queueName]
-			if !ok {
-				//Proceed on a queue that doesn't exist anymore.
-				lock.Unlock()
-				continue
-			}
-
-			if queue.Len() == 0 {
-				//last command on this queue exited successfully.
-				//we can safely delete it.
-				log.Println("Queue", queueName, "is empty, cleaning up...")
-				delete(mgr.queues, queueName)
-				lock.Unlock()
-				continue
-			}
-
-			lock.Unlock()
-
-			next := queue.Remove(queue.Front()).(*Cmd)
-			mgr.producer <- next
-		}
-	}()
+	go mgr.producerLoop()
 
 	return mgr
+}
+
+func (mgr *cmdQueueManager) dispatcherLoop() {
+	for {
+		cmd := <-mgr.consumer
+
+		queueName := cmd.Args.GetString("queue")
+		if queueName == "" {
+			//not a queued command just log a warning and continue
+			log.Println("WARNING: Queue manager received a command with no set queue", cmd)
+			continue
+		}
+
+		log.Println("Pushing command to queue", queueName)
+		//push command to correct queue
+		mgr.lock.Lock()
+		queue, ok := mgr.queues[queueName]
+		if !ok {
+			log.Println("Queue", queueName, "doesn't exist, initializing...")
+			queue = list.New()
+			mgr.queues[queueName] = queue
+		}
+		mgr.lock.Unlock()
+		//push the command to the queue.
+		queue.PushBack(cmd)
+
+		if !ok {
+			//since we just create this queue. We signal that it's ready
+			//to produce the first queued command. Think of it as intial start
+			//condition. When this command exists, it will auto signal the next command
+			//and so on.
+			mgr.signal <- queueName
+		}
+	}
+}
+
+//producer gets the next job to run and send it over channel
+func (mgr *cmdQueueManager) producerLoop() {
+	for {
+		queueName := <-mgr.signal
+
+		log.Println("Queue", queueName, "ready...")
+		mgr.lock.Lock()
+		queue, ok := mgr.queues[queueName]
+		if !ok {
+			//Proceed on a queue that doesn't exist anymore.
+			mgr.lock.Unlock()
+			continue
+		}
+
+		if queue.Len() == 0 {
+			//last command on this queue exited successfully.
+			//we can safely delete it.
+			log.Println("Queue", queueName, "is empty, cleaning up...")
+			delete(mgr.queues, queueName)
+			mgr.lock.Unlock()
+			continue
+		}
+
+		mgr.lock.Unlock()
+
+		next := queue.Remove(queue.Front()).(*Cmd)
+		mgr.producer <- next
+	}
 }
 
 func (qm *cmdQueueManager) Push(cmd *Cmd) {
